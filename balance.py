3bb449ef5f28bcb361e29dc007d1b3dc53b2e323
Mahan <mahan.rafiei@yahoo.com>
2020-11-01 00:12:34 +0330

    BRWN

--- balance.py
+++ balance.py
@@ -29,9 +29,9 @@ class Balance(commands.Cog):
                 icon_url=ctx.author.avatar_url_as(format='png', size=256))
             embed.add_field(
                 name="First of all, please type `//help`",
-                value="Welcome to world of Tip Sugar !")
+                value="Welcome to world of Tip BRWN !")
             embed.set_thumbnail(url=self.bot.user.avatar_url_as(format='png', size=1024))
-            embed.set_footer(text="Tip Sugar {0} [Owner: {1}]".format(config.VERSION, self.bot.get_user(config.OWNER_ID)),
+            embed.set_footer(text="Tip BRWN {0} [Owner: {1}]".format(config.VERSION, self.bot.get_user(config.OWNER_ID)),
                              icon_url=self.bot.user.avatar_url_as(format='png', size=256))
 
             await ctx.channel.send(embed=embed)
@@ -51,9 +51,9 @@ class Balance(commands.Cog):
                 name=user_name,
                 icon_url=ctx.author.avatar_url_as(format='png', size=256))
             embed.add_field(
-                name="{0} SUGAR".format(str(balance)),
-                value="unconfirmed : {0} SUGAR".format(str(unconfirmed_balance)))
-            embed.set_footer(text="Tip Sugar {0} [Owner: {1}]".format(config.VERSION, self.bot.get_user(config.OWNER_ID)),
+                name="{0} BRWN".format(str(balance)),
+                value="unconfirmed : {0} BRWN".format(str(unconfirmed_balance)))
+            embed.set_footer(text="Tip BRWN {0} [Owner: {1}]".format(config.VERSION, self.bot.get_user(config.OWNER_ID)),
                              icon_url=self.bot.user.avatar_url_as(format='png', size=256))
 
             await ctx.channel.send(embed=embed)
