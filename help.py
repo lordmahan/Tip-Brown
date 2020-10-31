e763483d648a1e5ebeb1d881c505eb650cea52ec
Mahan <mahan.rafiei@yahoo.com>
2020-11-01 00:14:41 +0330

    BRWN

--- help.py
+++ help.py
@@ -18,7 +18,7 @@ class Help(commands.Cog):
             user_db.add_user(user_id, user_name)
 
             embed = discord.Embed(
-                title="**Sugar Bot Command List:**",
+                title="**BRWN Bot Command List:**",
                 color=0x7152b6)
             embed.set_author(
                 name=ctx.author.display_name,
@@ -29,27 +29,27 @@ class Help(commands.Cog):
                 inline=True)
             embed.add_field(
                 name="**//info**",
-                value="Show Sugar Core wallet/blockchain info.",
+                value="Show BRWN Core wallet/blockchain info.",
                 inline=True)
             embed.add_field(
                 name="**//balance**",
-                value="Show your Sugar balances.",
+                value="Show your BRWN balances.",
                 inline=True)
             embed.add_field(
                 name="**//deposit**",
-                value="Show your Sugar deposit address.",
+                value="Show your BRWN deposit address.",
                 inline=True)
             embed.add_field(
                 name="**//tip**",
-                value="Tip specified user. [//tip @BeeHIVE Network 1] to tip 1 SUGAR",
+                value="Tip specified user. [//tip @User 1] to tip 1 BRWN",
                 inline=True)
             embed.add_field(
                 name="**//withdraw**",
-                value="Withdraw Sugar from your wallet. [//withdraw ADDRESS AMOUNT]",
+                value="Withdraw BRWN from your wallet. [//withdraw ADDRESS AMOUNT]",
                 inline=True)
             embed.add_field(
                 name="**//withdrawall**",
-                value="Withdraw all Sugar from your wallet. [//withdrawall ADDRESS]",
+                value="Withdraw all BRWN from your wallet. [//withdrawall ADDRESS]",
                 inline=True)
             embed.set_thumbnail(url=self.bot.user.avatar_url_as(format='png', size=1024))
             embed.set_footer(text="TipBot v{0}".format(config.VERSION), icon_url=self.bot.user.avatar_url_as(format='png', size=256))
@@ -59,7 +59,7 @@ class Help(commands.Cog):
             pass
 
             embed = discord.Embed(
-                title="**Sugar Bot Command List:**",
+                title="**BRWN Bot Command List:**",
                 color=0x7152b6)
             embed.set_author(
                 name=ctx.author.display_name,
@@ -70,27 +70,27 @@ class Help(commands.Cog):
                 inline=True)
             embed.add_field(
                 name="**//info**",
-                value="Show Sugar Core wallet/blockchain info.",
+                value="Show BRWN Core wallet/blockchain info.",
                 inline=True)
             embed.add_field(
                 name="**//balance**",
-                value="Show your Sugar balances.",
+                value="Show your BRWN balances.",
                 inline=True)
             embed.add_field(
                 name="**//deposit**",
-                value="Show your Sugar deposit address.",
+                value="Show your BRWN deposit address.",
                 inline=True)
             embed.add_field(
                 name="**//tip**",
-                value="Tip specified user. [//tip @BeeHIVE Network 1] to tip 1 SUGAR",
+                value="Tip specified user. [//tip @USER 1] to tip 1 BRWN",
                 inline=True)
             embed.add_field(
                 name="**//withdraw**",
-                value="Withdraw Sugar from your wallet. [//withdraw ADDRESS AMOUNT]",
+                value="Withdraw BRWN from your wallet. [//withdraw ADDRESS AMOUNT]",
                 inline=True)
             embed.add_field(
                 name="**//withdrawall**",
-                value="Withdraw all Sugar from your wallet. [//withdrawall ADDRESS]",
+                value="Withdraw all BRWN from your wallet. [//withdrawall ADDRESS]",
                 inline=True)
             embed.set_thumbnail(url=self.bot.user.avatar_url_as(format='png', size=1024))
             embed.set_footer(text="TipBot v{0}".format(config.VERSION), icon_url=self.bot.user.avatar_url_as(format='png', size=256))
